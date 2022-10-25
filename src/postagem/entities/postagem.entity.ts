5b60b2a29683f409efd68bd135429516e79fe1da
raffaez <rafaeleasouzas@gmail.com>
2022-10-25 08:13:11 -0300

    update(postagem): implements relations with user

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,6 +1,7 @@
 import { IsNotEmpty } from "class-validator";
 import { Tema } from "src/tema/entities/tema.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Usuario } from "src/usuario/entities/usuario.entity";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({name: 'tb_postagens'})
 export class Postagem{
@@ -23,4 +24,9 @@ export class Postagem{
         onDelete: "CASCADE"
     })
     tema: Tema;
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: 'CASCADE'
+    })
+    usuario: Usuario;
 }
--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -1,10 +1,11 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { TemaService } from "src/tema/services/tema.service";
-import { TemaModule } from "src/tema/tema.module";
-import { PostagemController } from "./controllers/postagem.controller";
-import { Postagem } from "./entities/postagem.entity";
-import { PostagemService } from "./services/postagem.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TemaService } from 'src/tema/services/tema.service';
+import { TemaModule } from 'src/tema/tema.module';
+
+import { PostagemController } from './controllers/postagem.controller';
+import { Postagem } from './entities/postagem.entity';
+import { PostagemService } from './services/postagem.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],
