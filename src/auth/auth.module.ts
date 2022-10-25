5dd217a91faf5e325eeb563311b4fe4650499e68
raffaez <rafaeleasouzas@gmail.com>
2022-10-25 08:12:22 -0300

    feat(auth): implements auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,6 @@
-import { Module } from "@nestjs/common";
-import { Bcrypt } from "./bcrypt/bcrypt";
+import { Module } from '@nestjs/common';
+
+import { Bcrypt } from './bcrypt/bcrypt';
 
 @Module({
   imports: [],
